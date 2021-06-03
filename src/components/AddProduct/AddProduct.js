36386ce3b095f9dc22cf3e7ad08e2dd5131a7718
Latifur Rahman <latifurrahman350@gmail.com>
2021-06-04 01:19:06 +0600

    some worked. no more today.good n8

--- src/components/AddProduct/AddProduct.js
+++ src/components/AddProduct/AddProduct.js
@@ -1,11 +1,21 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
 import './AddProduct.css';
 
 const AddProduct = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+    const [imageURL, setImageURL] = useState(null);
+
+    const onSubmit = data => {
+        const productData = {
+            name: data.name,
+            price: data.price,
+            imageURL: imageURL
+        }
+        console.log(data)
+    };
+
     const handleUploadImage = event => {
         console.log(event.target.files[0])
         const imageData = new FormData();
@@ -14,7 +24,7 @@ const AddProduct = () => {
         axios.post('https://api.imgbb.com/1/upload', 
         imageData)
             .then(function (response) {
-                console.log(response.data.data.display_url);
+                setImageURL(response.data.data.display_url);
             })
             .catch(function (error) {
                 console.log(error);
@@ -23,17 +33,24 @@ const AddProduct = () => {
     return (
         <form onSubmit={handleSubmit(onSubmit)} className="container mt-1 form-container">
             <h1 className="add-product-title">Add Product</h1>
-            <h5>Product Name</h5>
-            <input defaultValue="test" className="" />
-            <br /> <br />
-            <h5>Add Price</h5>
-            <input defaultValue="test"  />
-            <br /> <br />
-            <h5>Add Photo</h5>
-            <input type="file" onChange={handleUploadImage} />
-            <br />
-            <br />
-            <input type="submit" value="Save" className="btn btn-danger"/>
+            <div className="row">
+                <div className="col-md-4">
+                    <h5 className="product-title">Product Name</h5>
+                    <input defaultValue="" {...register("name")} placeholder="Enter name" className="form-input" />
+                </div>
+                <div className="col-md-4">
+                    <h5 className="product-title">Add Price</h5>
+                    <input defaultValue="" {...register("price")} type="number" placeholder="Enter price" className="form-input" />
+                </div>
+                <div className="col-md-3 ">
+                    <h5 className="product-title">Add Photo</h5>
+                    <input type="file" onChange={handleUploadImage} />
+                </div>
+                <div className="col-md-1">
+                    <input type="submit" value="Save" className="btn btn-danger mt-3" />
+                </div>
+            </div>
+            
         </form>
     );
 };
